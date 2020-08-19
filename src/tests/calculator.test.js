13d78327901cb5a122f2fa930df8387ca510ac40
Elbie-em <emoonga1993@gmail.com>
2020-08-19 23:20:23 +0200

    Add calculator test for multiply

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -3,3 +3,5 @@ const calculator = require('../modules/calculator');
 test('calculates the sum of two numbers',() => expect(calculator.add(3,2)).toBe(5));
 
 test('calculates the difference of two numbers', () => expect(calculator.subtract(10,5)).toBe(5));
+
+test('calculates the product of two numbers', () => expect(calculator.multiply(6,6)).toBe(36));
