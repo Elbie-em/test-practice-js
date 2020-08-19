27c8f0b01e900cdccd517e4d7a46d203326bbc48
Elbie-em <emoonga1993@gmail.com>
2020-08-19 23:21:22 +0200

    Add multiply method in calculator module

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -3,10 +3,13 @@ const calculator = (() => {
 	const add = (a,b) => a + b;
 
 	const subtract = (a,b) => a - b;
+
+	const multiply = (a,b) => a * b;
 	
 	return {
 		add,
-		subtract
+		subtract,
+		multiply
 	}
 
 })();
