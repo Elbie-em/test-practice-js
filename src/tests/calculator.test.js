b0214c56e2758dae7af7f7dab17ea2a5f89a2596
Elbie-em <emoonga1993@gmail.com>
2020-08-19 23:14:11 +0200

    Add calculator test for subtract

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -1,3 +1,5 @@
 const calculator = require('../modules/calculator');
 
 test('adds two numbers',() => expect(calculator.add(3,2)).toBe(5));
+
+test('calculates the difference of two numbers', () => expect(calculator.subtract(10,5)).toBe(5));
