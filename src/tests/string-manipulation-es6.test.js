7aa34457c75abfad305fd18e75e8004fe270000d
Abdel <juniorperezpy@gmail.com>
2020-08-20 11:07:39 -0400

    mend

--- src/tests/string-manipulation-es6.test.js
+++ src/tests/string-manipulation-es6.test.js
@@ -2,4 +2,4 @@ import { capitalize, reverseString } from '../modules/string-manipulation';
 
 it('capitalizes string', () => expect(capitalize('string')).toBe('String'));
 
-it('reverses string', () => expect(reverseString('string')).toBe('gnirts'));
+it('reverses string', () => expect(reverseString('StriNg')).toBe('gNirtS'));
